9f7c67b617f203c3e220ebbb5377dbbda5bc22fc
ZeynepOzan <ozanzzeynep@gmail.com>
2023-12-13 15:43:30 +0300

    request and response updated

--- pair-9/src/main/java/com/tobeto/pair9/core/utilities/mappers/ModelMapperService.java
+++
@@ -1,9 +0,0 @@
-package com.tobeto.pair9.core.utilities.mappers;
-
-import org.modelmapper.ModelMapper;
-
-public interface ModelMapperService {
-    ModelMapper forResponse();
-    ModelMapper forRequest();
-
-}
---
+++ rentACar-main/pair-9/src/main/java/com/tobeto/pair9/controllers/carController.java
@@ -0,0 +1,47 @@
+package com.tobeto.pair9.controllers;
+
+import com.tobeto.pair9.entities.Car;
+import com.tobeto.pair9.services.abstracts.BrandService;
+import com.tobeto.pair9.services.abstracts.CarService;
+import com.tobeto.pair9.services.dtos.car.requests.AddCarRequest;
+import com.tobeto.pair9.services.dtos.car.requests.UpdateCarRequest;
+import com.tobeto.pair9.services.dtos.car.responses.GetByIdCarResponse;
+import com.tobeto.pair9.services.dtos.car.responses.GetListCarResponse;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/cars")
+public class carController {
+
+    private CarService carService;
+
+    @GetMapping()
+    public List<GetListCarResponse> getAll(){
+        return carService.getAll();
+    }
+    @GetMapping("/{id}")
+    public GetByIdCarResponse getById(@PathVariable int id){
+        return carService.getById(id);
+    }
+
+    @PostMapping("/add")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public void add(@RequestBody @Valid AddCarRequest request){
+        carService.add(request);
+    }
+    @PutMapping("/{id}")
+    public void update(@RequestBody @Valid UpdateCarRequest request){
+        this.carService.update(request);
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id){
+        this.carService.delete(id);
+    }
+}
