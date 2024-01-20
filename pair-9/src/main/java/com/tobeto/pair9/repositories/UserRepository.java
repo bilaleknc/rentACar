bf290bc66442250e0d6201c93ebda818e8f83890
ZeynepOzan <ozanzzeynep@gmail.com>
2024-01-20 17:53:38 +0300

    security-jwt updated

--- pair-9/src/main/java/com/tobeto/pair9/repositories/UserRepository.java
+++ pair-9/src/main/java/com/tobeto/pair9/repositories/UserRepository.java
@@ -3,8 +3,14 @@ package com.tobeto.pair9.repositories;
 import com.tobeto.pair9.entities.concretes.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Integer> {
 
     boolean existsUserByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsById(int id);
 
 }
