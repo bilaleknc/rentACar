75284819d283d157623de944d2c74245b3822d9c
Bilal EKİNCİ <etkhckrone@gmail.com>
2024-01-28 17:37:24 +0300

    changed request dto

--- pair-9/src/main/java/com/tobeto/pair9/controllers/UsersController.java
+++ pair-9/src/main/java/com/tobeto/pair9/controllers/UsersController.java
@@ -15,25 +15,26 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin
 public class UsersController {
 
     private final UserService userService;
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
 
-    @PostMapping
+    @PostMapping("/signUp")
     @ResponseStatus(HttpStatus.CREATED)
     private void register(@RequestBody CreateUserRequest request){
         userService.register(request);
     }
 
-    @PostMapping("login")
+    @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
     public String login(@RequestBody LoginRequest loginRequest){
         //Aut Service taşınmalı
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),loginRequest.getPassword()));
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUserName(),loginRequest.getPassword()));
         if(authentication.isAuthenticated()){
-            return jwtService.generateToken(loginRequest.getUsername());
+            return jwtService.generateToken(loginRequest.getUserName());
         }
         throw new RuntimeException("Kullanıcı adı yada şifra hatalı");
     }
